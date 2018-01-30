c94b8054b7ecb45c880519a61732a5f6c437fbeb
BelyaevaSveta <33847312+BelyaevaSveta@users.noreply.github.com>
2018-01-30 15:57:15 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -6,6 +6,7 @@ import st04.main
 import st05.main
 import st06.main
 import st07.main
+import st08.main
 import st09.main
 import st10.main
 import st12.main
@@ -53,6 +54,7 @@ MENU = [
         ["[05] Баганов", st05.main.main],
 		["[06] Батищев", st06.main.main],
         ["[07] Белова", st07.main.main],
+	["[08] Беляева",st08.main.main],
         ["[09] Беркита", st09.main.main],
         ["[10] Бледных", st10.main.main],
         ["[12] Бреткин", st12.main.main],
